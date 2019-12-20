1101b2397d88e63b0e0fbee17362a26ddca4a941
dokyun <cs.dkkim@gmail.com>
2019-12-20 16:48:12 +0900

    출처 및 라이선스 표기

--- app/src/main/java/com/example/kcalmera/ImageClassifier.java
+++ app/src/main/java/com/example/kcalmera/ImageClassifier.java
@@ -1,3 +1,18 @@
+/** Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
 package com.example.kcalmera;
 
 import android.app.Activity;
@@ -49,8 +64,8 @@ public class ImageClassifier {
     static final int DIM_IMG_SIZE_Y = 224;
 
     // inception
-   // static final int DIM_IMG_SIZE_X = 299;
-  //  static final int DIM_IMG_SIZE_Y = 299;
+    // static final int DIM_IMG_SIZE_X = 299;
+    // static final int DIM_IMG_SIZE_Y = 299;
 
     private static final int IMAGE_MEAN = 0;
     private static final float IMAGE_STD = 256.0f;
